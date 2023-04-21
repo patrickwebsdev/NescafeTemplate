f162fcf9da32d4be38109fcee8392c26f2cf58bf
Patricio Almada <patricioalmada911@gmail.com>
2023-04-21 03:25:32 -0300

    Scroll bar function

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 const scrollPage = new ScrollPage("#screen__scroll", {
-      // easing functions
-      animation: "easeInSine",
-      // animation speed
-      time: 500,
-      // show/hide scrollbar
+      time: 0,
       scrollBar: false,
-      // css class name for active page element
-      pageSelectedClass:"active",
-      // css class name for active menu item
-      menuSelectedClass:"active",
-      // trigger scrollpage if scrolling content reach bottom or top
+      menu: "div.screen__nav",
+      menuSelectedClass:"screen__dot--active",
       triggerScrollChildren: false,
-      // relative to parent height or nor
       relative: false,
-});
-scrollPage.onFinish(function(e){
-    const dots = document.querySelectorAll('.screen__dot');
-    Array.from(dots).forEach((dot) => {
-        dot.classList.remove('screen__dot--active');
-    })
-    dots[e.currentPageName-1].classList.add('screen__dot--active');
 });
